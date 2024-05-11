5979318234652022b7e782cca4bb76774b77a39a
Moritz Jüttner <dermoritz2411@gmail.com>
2024-05-11 11:36:32 +0200

    Comment Update

--- main.py
+++ main.py
@@ -33,27 +33,31 @@ text_list = []
 word_list = []
 
 
-# Open About on About Button press
+# Open About Window
 def about_open():
     about.show()
     pass
 
 
+# Open Type (T2T/T2I/LText) Chooser for Opening Sets
 def open_type_open():
     open_type.show()
     pass
 
 
+# Open Set Creator Window
 def creator_open():
     creator.show()
     pass
 
 
+# Open Editor Type Chooser
 def editor_director_open():
     editor_director.show()
     pass
 
 
+# Open Editor for T2T Sets
 def editor_t2t_open():
     try:
         editor_t2t_init()
@@ -63,6 +67,7 @@ def editor_t2t_open():
     pass
 
 
+# Open Editor for T2I Sets
 def editor_t2i_open():
     try:
         editor_t2i_init()
@@ -72,6 +77,7 @@ def editor_t2i_open():
     pass
 
 
+# Open Editor for LText Sets
 def editor_ltext_open():
     try:
         editor_ltext_init()
@@ -83,7 +89,7 @@ def editor_ltext_open():
     pass
 
 
-# Set selection on Open Set Button press
+# Open File Chooser for Definition Sets
 def definitions_open():
     global set_name
     # File Dialog for Set Select
@@ -91,60 +97,66 @@ def definitions_open():
     definitions_init()  # Call Innit Function
 
 
+# Open File Chooser for T2I Sets
 def pictures_open():
     global set_name
     set_name = str(QFileDialog.getOpenFileName(open_type, "Select File", "sets/T2I", filter="*.csv"))
     pictures_init()
 
 
+# Open File Chooser for LText Sets
 def ltext_open():
     global set_name
     set_name = str(QFileDialog.getOpenFileName(open_type, "Select File", "sets/LText", filter="*.txt"))
     ltext_init()
 
 
+# Create Directories and Files for new Sets
 def creator_create():
-    if creator_ui.type_input.currentIndex() == 0:
-        path = "sets/T2T/" + creator_ui.subject_input.text() + "/"
-        if not os.path.exists(path):
+    if creator_ui.type_input.currentIndex() == 0:  # When Type is T2T
+        path = "sets/T2T/" + creator_ui.subject_input.text() + "/"  # Path consisting of Type and Subject
+        if not os.path.exists(path):  # If the Path does not exist, create it
             os.mkdir(path, mode=0o777)
-        with open(path + creator_ui.name_input.text() + ".csv", "a+"):
+        with open(path + creator_ui.name_input.text() + ".csv", "a+"):  # Create an empty .csv file at the Path
             pass
-    if creator_ui.type_input.currentIndex() == 1:
-        path = "sets/T2I/" + creator_ui.subject_input.text() + "/"
-        if not os.path.exists(path):
+    if creator_ui.type_input.currentIndex() == 1:  # When Type is T2I
+        path = "sets/T2I/" + creator_ui.subject_input.text() + "/"  # Path consisting of Type and Subject
+        if not os.path.exists(path):  # If the Path does not exist, create it
             os.mkdir(path, mode=0o777)
-        with open(path + creator_ui.name_input.text() + ".csv", "a+"):
+        with open(path + creator_ui.name_input.text() + ".csv", "a+"):  # Create an empty .csv file at the Path
             pass
-    if creator_ui.type_input.currentIndex() == 2:
-        path = "sets/LText/" + creator_ui.subject_input.text() + "/"
-        if not os.path.exists(path):
+    if creator_ui.type_input.currentIndex() == 2:  # When Type is LText
+        path = "sets/LText/" + creator_ui.subject_input.text() + "/"  # Path consisting of Type and Subject
+        if not os.path.exists(path):  # If the Path does not exist, create it
             os.mkdir(path, mode=0o777)
-        with open(path + creator_ui.name_input.text() + ".txt", "a+"):
+        with open(path + creator_ui.name_input.text() + ".txt", "a+"):  # Create an empty .txt file at the Path
             pass
     creator.close()
     pass
 
 
+# Edit T2T Sets
 def editor_t2t_init():
     global file
-    file = str(QFileDialog.getOpenFileName(filter="*.csv", directory="sets/T2T"))
-    content = csv_to_lists(file)
-    editor_t2t_ui.text_area.setText("")
-    for i in range(0, len(content)):
+    file = str(QFileDialog.getOpenFileName(filter="*.csv", directory="sets/T2T"))  # Get the to be edited File
+    content = csv_to_lists(file)  # Give File Content into a Var
+    editor_t2t_ui.text_area.setText("")  # Clear the Text-Area
+    for i in range(0, len(content)):  # Add the Content to the Text-Area
         editor_t2t_ui.text_area.append(str(content[i][0]) + "; " + str(content[i][1]))
     pass
 
 
+# Save the Edited T2T Set to the File
 def editor_t2t_save():
     global file
     save = open(file.strip("()").split(",")[0].strip("'"), "w")
-    save.write(editor_t2t_ui.text_area.toPlainText())
+    save.write(editor_t2t_ui.text_area.toPlainText())  # Write the Content of the Text-Area to the File
     save.close()
     editor_t2t.close()
     pass
 
 
+# Edit T2I Sets (same as for T2T Sets)
 def editor_t2i_init():
     global file
     file = str(QFileDialog.getOpenFileName(filter="*.csv", directory="sets/T2I"))
@@ -155,6 +167,7 @@ def editor_t2i_init():
     pass
 
 
+# Save the Edited T2I Set File to the File (same as for T2I Files)
 def editor_t2i_save():
     global file
     save = open(file.strip("()").split(",")[0].strip("'"), "w")
@@ -164,6 +177,7 @@ def editor_t2i_save():
     pass
 
 
+# Edit LText Sets (Similar to the first two, but with a Stream and not the CSV Func)
 def editor_ltext_init():
     global file
     file = str(QFileDialog.getOpenFileName(filter="*.txt", directory="sets/LText"))
@@ -175,6 +189,7 @@ def editor_ltext_init():
     pass
 
 
+# Save the Edited LText Set to the File (Same as for the first two)
 def editor_ltext_save():
     global file
     save = open(file.strip("()").split(",")[0].strip("'"), "w")
@@ -200,18 +215,18 @@ def pictures_init():
         def2 = items[1]
         def3 = items[2]
 
-        # Assign the 3 corresponding shuffled Items(Terms)
+        # Assign the 3 corresponding shuffled Items(Pics)
         pic1 = items[shuffle[0]]
         pic2 = items[shuffle[1]]
         pic3 = items[shuffle[2]]
 
-        pictures_ui.term1_text.setText(data[def1][0])  # Assigning chosen text to Buttons
-        pictures_ui.term1_button.setToolTip(str(def1))  # Assigning Index as Tooltip for later use
-        pictures_ui.term1_button.setVisible(True)  # In case of refresh
+        pictures_ui.term1_text.setText(data[def1][0])  # Set Text on the Label
+        pictures_ui.term1_button.setToolTip(str(def1))  # Set Tool Tip as Identifier
+        pictures_ui.term1_button.setVisible(True)  # Incase hidden, make Visible
         pictures_ui.term1_text.setVisible(True)
         pictures_ui.term1_panel.setVisible(True)
-        pictures_ui.term1_button.setChecked(False)  # In case of refresh
-        pictures_ui.pic1_button.setIcon(QIcon(str(data[pic1][1]) + ".jpg"))  # Assign Pics
+        pictures_ui.term1_button.setChecked(False)  # Incase checked, make unchecked
+        pictures_ui.pic1_button.setIcon(QIcon(str(data[pic1][1]) + ".jpg"))  # Set Picture on the Label
         pictures_ui.pic1_button.setToolTip(str(pic1))
         pictures_ui.pic1_button.setVisible(True)
         pictures_ui.pic1_panel.setVisible(True)
@@ -251,10 +266,40 @@ def pictures_pic_button_clicked(index, number):
     global pictures_term_name_number
     global pictures_pic_name_number
     global correct_counter
-    pictures_pic_button_is_clicked = index
-    pictures_pic_name_number = number
+    pictures_pic_button_is_clicked = index  # The Index of the Picture as assigned above with the ToolTip
+    pictures_pic_name_number = number  # The Number of the Button Component
 
-    if pictures_term_button_is_clicked is not None:
+    if pictures_term_button_is_clicked is not None:  # If this is the second clicked Button
+        if pictures_term_button_is_clicked == pictures_pic_button_is_clicked:  # And if the two clicked Buttons are the same
+            pictures_term_button_is_clicked = None  # Reset both Buttons
+            pictures_pic_button_is_clicked = None
+            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_button").setVisible(False)  # Hide the Components
+            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_text").setVisible(False)
+            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_panel").setVisible(False)
+            getattr(pictures_ui, "pic" + str(pictures_pic_name_number) + "_button").setVisible(False)
+            getattr(pictures_ui, "pic" + str(pictures_pic_name_number) + "_panel").setVisible(False)
+            correct_counter = correct_counter + 1  # Up the Counter for the Refresh
+            if correct_counter == 3:  # Refresh the Window when all Buttons have been clicked correctly
+                pictures_init()
+                correct_counter = 0
+        else:
+            pictures_term_button_is_clicked = None  # Reset both Buttons and uncheck them if the Buttons are not the same
+            pictures_pic_button_is_clicked = None
+            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_button").setChecked(False)
+            getattr(pictures_ui, "pic" + str(pictures_pic_name_number) + "_button").setChecked(False)
+    pass
+
+
+def pictures_term_button_clicked(index, number):  # Same as above but reversed
+    global pictures_term_button_is_clicked
+    global pictures_pic_button_is_clicked
+    global pictures_term_name_number
+    global pictures_pic_name_number
+    global correct_counter
+    pictures_term_button_is_clicked = index
+    pictures_term_name_number = number
+
+    if pictures_pic_button_is_clicked is not None:
         if pictures_term_button_is_clicked == pictures_pic_button_is_clicked:
             pictures_term_button_is_clicked = None
             pictures_pic_button_is_clicked = None
@@ -275,65 +320,31 @@ def pictures_pic_button_clicked(index, number):
     pass
 
 
-def pictures_term_button_clicked(index, number):
-    global pictures_term_button_is_clicked  # Set-Index of last pressed Term-Button parsed as ToolTip
-    global pictures_pic_button_is_clicked  # Set-Index of last pressed Def-Button parsed as ToolTip
-    global pictures_term_name_number  # Number in Term-Button Name
-    global pictures_pic_name_number  # Number in Def-Button Name
-    global correct_counter  # Counter for refresh trigger
-    pictures_term_button_is_clicked = index  # Assign parsed Index to var
-    pictures_term_name_number = number  # Assign Name Number
-
-    if pictures_pic_button_is_clicked is not None:  # If this is the second click
-        if pictures_term_button_is_clicked == pictures_pic_button_is_clicked:  # If both buttons have the same Index
-            # (are corresponding)
-            pictures_term_button_is_clicked = None  # Reset Index Vars
-            pictures_pic_button_is_clicked = None
-            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_button").setVisible(False)
-            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_text").setVisible(False)
-            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_panel").setVisible(False)
-            getattr(pictures_ui, "pic" + str(pictures_pic_name_number) + "_button").setVisible(False)
-            getattr(pictures_ui, "pic" + str(pictures_pic_name_number) + "_panel").setVisible(False)
-            correct_counter = correct_counter + 1  # Counter Count-Up
-            if correct_counter == 3:  # Check if time for refresh
-                pictures_init()  # Run Refresh function
-                correct_counter = 0  # Reset counter
-        else:  # If not corresponding
-            pictures_term_button_is_clicked = None  # Reset so no false checks happen
-            pictures_pic_button_is_clicked = None
-            getattr(pictures_ui, "term" + str(pictures_term_name_number) + "_button").setChecked(False)  # QOL reset
-            getattr(pictures_ui, "pic" + str(pictures_pic_name_number) + "_button").setChecked(False)
-    pass
-
-
-# Innit on Set Select/Refresh on new Round for Definitions
-def definitions_init():
+def definitions_init():  # Same as above but with Text instead of Pictures
     open_type.close()
-    global set_name  # Selected Set from definitions_open()
+    global set_name
     global data
     try:
-        data = csv_to_lists(set_name)  # Call CSV function (see below)
-        items = list(range(0, len(data)))  # Index of all Elements from data
-        shuffle = [0, 1, 2]  # Shuffle-List for 2nd assignment
-        random.shuffle(items)  # Shuffling both lists for randomness
+        data = csv_to_lists(set_name)
+        items = list(range(0, len(data)))
+        shuffle = [0, 1, 2]
+        random.shuffle(items)
         random.shuffle(shuffle)
 
-        # Assign first 3 Items(Definitions) from Index to buttons
         def1 = items[0]
         def2 = items[1]
         def3 = items[2]
 
-        # Assign the 3 corresponding shuffled Items(Terms)
         term1 = items[shuffle[0]]
         term2 = items[shuffle[1]]
         term3 = items[shuffle[2]]
 
-        definitions_ui.def1_text.setText(data[def1][0])  # Assigning chosen text to Buttons
-        definitions_ui.def1_button.setToolTip(str(def1))  # Assigning Index as Tooltip for later use
-        definitions_ui.def1_button.setVisible(True)  # In case of refresh
+        definitions_ui.def1_text.setText(data[def1][0])
+        definitions_ui.def1_button.setToolTip(str(def1))
+        definitions_ui.def1_button.setVisible(True)
         definitions_ui.def1_text.setVisible(True)
         definitions_ui.def1_panel.setVisible(True)
-        definitions_ui.def1_button.setChecked(False)  # In case of refresh
+        definitions_ui.def1_button.setChecked(False)
         definitions_ui.term1_text.setText(data[term1][1])
         definitions_ui.term1_button.setToolTip(str(term1))
         definitions_ui.term1_button.setVisible(True)
@@ -371,8 +382,7 @@ def definitions_init():
     pass
 
 
-# Checks on Button clicked (see below)
-def definitions_def_button_clicked(index, number):
+def definitions_def_button_clicked(index, number):  # Same as with T2I (Above)
     global term_button_is_clicked
     global def_button_is_clicked
     global term_name_number
@@ -403,20 +413,19 @@ def definitions_def_button_clicked(index, number):
     pass
 
 
-def definition_term_button_clicked(index, number):
-    global term_button_is_clicked  # Set-Index of last pressed Term-Button parsed as ToolTip
-    global def_button_is_clicked  # Set-Index of last pressed Def-Button parsed as ToolTip
-    global term_name_number  # Number in Term-Button Name
-    global def_name_number  # Number in Def-Button Name
-    global correct_counter  # Counter for refresh trigger
-    term_button_is_clicked = index  # Assign parsed Index to var
-    term_name_number = number  # Assign Name Number
+def definition_term_button_clicked(index, number):   # Same as with T2I (Above)
+    global term_button_is_clicked
+    global def_button_is_clicked
+    global term_name_number
+    global def_name_number
+    global correct_counter
+    term_button_is_clicked = index
+    term_name_number = number
 
-    if def_button_is_clicked is not None:  # If this is the second click
-        if term_button_is_clicked == def_button_is_clicked:  # If both buttons have the same Index (are corresponding)
-            term_button_is_clicked = None  # Reset Index Vars
+    if def_button_is_clicked is not None:
+        if term_button_is_clicked == def_button_is_clicked:
+            term_button_is_clicked = None
             def_button_is_clicked = None
-            # Hiding Buttons for User Feedback
             getattr(definitions_ui, "term" + str(term_name_number) + "_button").setVisible(False)
             getattr(definitions_ui, "term" + str(term_name_number) + "_text").setVisible(False)
             getattr(definitions_ui, "term" + str(term_name_number) + "_panel").setVisible(False)
@@ -424,14 +433,14 @@ def definition_term_button_clicked(index, number):
             getattr(definitions_ui, "def" + str(def_name_number) + "_text").setVisible(False)
             getattr(definitions_ui, "def" + str(def_name_number) + "_panel").setVisible(False)
 
-            correct_counter = correct_counter + 1  # Counter Count-Up
-            if correct_counter == 3:  # Check if time for refresh
-                definitions_init()  # Run Refresh function
-                correct_counter = 0  # Reset counter
-        else:  # If not corresponding
-            term_button_is_clicked = None  # Reset so no false checks happen
+            correct_counter = correct_counter + 1
+            if correct_counter == 3:
+                definitions_init()
+                correct_counter = 0
+        else:
+            term_button_is_clicked = None
             def_button_is_clicked = None
-            getattr(definitions_ui, "term" + str(term_name_number) + "_button").setChecked(False)  # QOL reset
+            getattr(definitions_ui, "term" + str(term_name_number) + "_button").setChecked(False)
             getattr(definitions_ui, "def" + str(def_name_number) + "_button").setChecked(False)
     pass
 
@@ -442,19 +451,19 @@ def ltext_init():
     global text_list
     global word_list
     try:
-        o = open(set_name.strip("()").split(",")[0].strip("'"), "r")
-        content = o.readlines()
+        o = open(set_name.strip("()").split(",")[0].strip("'"), "r")  # Open the File
+        content = o.readlines()  # Get Content from the File
         o.close()
-        random_num = random_even_number(0, len(content)-1)
-        text_list = content[random_num].split()
-        word_list = content[random_num + 1].strip().split(", ")
+        random_num = random_even_number(0, len(content)-1)  # Get random Text
+        text_list = content[random_num].split()  # Split Words in Text
+        word_list = content[random_num + 1].strip().split(", ")  # Split Words List
 
-        for i in range(0, len(text_list)):
+        for i in range(0, len(text_list)):  # Remove Words in the Words List from the Text
             for j in range(0, len(word_list)):
                 if compare_strings(text_list[i], word_list[j]):
                     text_list[i] = "_____"
 
-        ltext_ui.words_input.clear()
+        ltext_ui.words_input.clear()  # Clear UI and Set Text
         ltext_ui.text_area.clear()
         output = ' '.join(text_list)
         ltext_ui.text_area.append(output)
@@ -466,15 +475,15 @@ def ltext_init():
 
 
 def ltext_check():
-    words = ltext_ui.words_input.text().split(", ")
-    if words == word_list:
-        ltext_init()
+    words = ltext_ui.words_input.text().split(", ")  # Get User generated Words
+    if words == word_list:  # If the words match
+        ltext_init()  # Refresh
     else:
-        ltext_ui.words_input.clear()
+        ltext_ui.words_input.clear()  # Clear Input Area
     pass
 
 
-def csv_to_lists(file_path):
+def csv_to_lists(file_path):  # Function to Open .csv File to a List
     try:
         with open(file_path.strip("()").split(",")[0].strip("'"), newline='', encoding="utf-8") as csv_file:
             reader = csv.reader(csv_file, delimiter=";")
@@ -484,22 +493,17 @@ def csv_to_lists(file_path):
         print("Please choose a file!")
 
 
-def compare_strings(string1, string2):
+def compare_strings(string1, string2):  # Function to... compare two Strings
     string1 = string1.rstrip(".")
     string1 = string1.rstrip(",")
     return string1 == string2
 
 
-def random_even_number(start, end):
+def random_even_number(start, end):  # Get a... random even number in a range (for LText)
     if end % 2 != 0:
         end -= 1
     num = random.randrange(start, end + 1, 2)
     return num
-
-
-def test():
-    print("Test")
-    pass
 
 
 app = QApplication([])
